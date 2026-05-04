9581f72eb04da84e1202b51b26d49480d0c266e0
Ian H. Pittwood <ian.pittwood@posit.co>
2026-05-04 12:30:52 -0600

    chore: fix shellcheck issues from pre-commit

--- workbench-for-google-cloud-workstations/2026.04/scripts/startup.sh
+++ workbench-for-google-cloud-workstations/2026.04/scripts/startup.sh
@@ -15,7 +15,7 @@ deactivate() {
     echo "== Exiting =="
     rstudio-server stop
     echo "Deactivating license ..."
-    ${LICENSE_MANAGER_PATH}/license-manager deactivate >/dev/null 2>&1
+    "${LICENSE_MANAGER_PATH}"/license-manager deactivate >/dev/null 2>&1
 
     echo "== Done =="
 }
@@ -23,9 +23,9 @@ trap deactivate EXIT
 
 verify_installation(){
    echo "==VERIFY INSTALLATION==";
-   mkdir -p $DIAGNOSTIC_DIR
-   chmod 777 $DIAGNOSTIC_DIR
-   rstudio-server verify-installation --verify-user=$RSW_TESTUSER | tee $DIAGNOSTIC_DIR/verify.log
+   mkdir -p "$DIAGNOSTIC_DIR"
+   chmod 777 "$DIAGNOSTIC_DIR"
+   rstudio-server verify-installation --verify-user="$RSW_TESTUSER" | tee "$DIAGNOSTIC_DIR"/verify.log
 }
 
 # Support RSP_ or RSW_ prefix
@@ -35,9 +35,9 @@ RSP_LICENSE_SERVER=${RSP_LICENSE_SERVER:-${RSW_LICENSE_SERVER}}
 # Activate License
 RSW_LICENSE_FILE_PATH=${RSW_LICENSE_FILE_PATH:-${RSP_LICENSE_FILE_PATH:-/etc/rstudio-server/license.lic}}
 if [ -n "$RSP_LICENSE" ]; then
-    ${LICENSE_MANAGER_PATH}/license-manager activate $RSP_LICENSE || true
+    "${LICENSE_MANAGER_PATH}"/license-manager activate "$RSP_LICENSE" || true
 elif [ -n "$RSP_LICENSE_SERVER" ]; then
-    ${LICENSE_MANAGER_PATH}/license-manager license-server $RSP_LICENSE_SERVER || true
+    "${LICENSE_MANAGER_PATH}"/license-manager license-server "$RSP_LICENSE_SERVER" || true
 elif test -f "${RSW_LICENSE_FILE_PATH}"; then
     rm -f /var/lib/rstudio-server/*.lic
     cp "${RSW_LICENSE_FILE_PATH}" /var/lib/rstudio-server/license.lic
@@ -58,14 +58,14 @@ unset RSW_LICENSE_FILE_PATH
 # Start Launcher
 if [ "$RSW_LAUNCHER" == "true" ]; then
   echo "Waiting for launcher to startup... to disable set RSW_LAUNCHER=false"
-  wait-for-it.sh localhost:5559 -t $RSW_LAUNCHER_TIMEOUT
+  wait-for-it.sh localhost:5559 -t "$RSW_LAUNCHER_TIMEOUT"
 fi
 
 # Check diagnostic configurations
 if [ "$DIAGNOSTIC_ENABLE" == "true" ]; then
   verify_installation
   if [ "$DIAGNOSTIC_ONLY" == "true" ]; then
-    echo $(<$DIAGNOSTIC_DIR/verify.log);
+    cat "$DIAGNOSTIC_DIR"/verify.log;
     echo "Exiting script because DIAGNOSTIC_ONLY=${DIAGNOSTIC_ONLY}";
     exit 0
   fi;
--- workbench-for-microsoft-azure-ml/template/scripts/startup.sh
+++ workbench-for-microsoft-azure-ml/template/scripts/startup.sh
@@ -12,7 +12,7 @@ deactivate() {
     echo "== Exiting =="
     rstudio-server stop
     echo "Deactivating license ..."
-    ${LICENSE_MANAGER_PATH}/license-manager deactivate >/dev/null 2>&1
+    "${LICENSE_MANAGER_PATH}"/license-manager deactivate >/dev/null 2>&1
 
     echo "== Done =="
 }
@@ -20,9 +20,9 @@ trap deactivate EXIT
 
 verify_installation(){
    echo "==VERIFY INSTALLATION==";
-   mkdir -p $DIAGNOSTIC_DIR
-   chmod 777 $DIAGNOSTIC_DIR
-   rstudio-server verify-installation --verify-user=$USER_NAME | tee $DIAGNOSTIC_DIR/verify.log
+   mkdir -p "$DIAGNOSTIC_DIR"
+   chmod 777 "$DIAGNOSTIC_DIR"
+   rstudio-server verify-installation --verify-user="$USER_NAME" | tee "$DIAGNOSTIC_DIR"/verify.log
 }
 
 # Backward compatibility for RSW_ and RSP_ prefixes
@@ -33,11 +33,11 @@ PWB_LICENSE_FILE_PATH=${PWB_LICENSE_FILE_PATH:-${RSW_LICENSE_FILE_PATH:-/etc/rst
 # Activate License
 PWB_LICENSE_FILE_PATH=${PWB_LICENSE_FILE_PATH:-/etc/rstudio-server/license.lic}
 if [ -n "$PWB_LICENSE" ]; then
-    /usr/lib/rstudio-server/bin/license-manager activate $PWB_LICENSE
+    /usr/lib/rstudio-server/bin/license-manager activate "$PWB_LICENSE"
 elif [ -n "$PWB_LICENSE_SERVER" ]; then
-    /usr/lib/rstudio-server/bin/license-manager license-server $PWB_LICENSE_SERVER
+    /usr/lib/rstudio-server/bin/license-manager license-server "$PWB_LICENSE_SERVER"
 elif test -f "$PWB_LICENSE_FILE_PATH"; then
-    /usr/lib/rstudio-server/bin/license-manager activate-file $PWB_LICENSE_FILE_PATH
+    /usr/lib/rstudio-server/bin/license-manager activate-file "$PWB_LICENSE_FILE_PATH"
 fi
 
 # ensure these cannot be inherited by child processes
@@ -49,14 +49,14 @@ unset RSW_LICENSE
 unset RSW_LICENSE_SERVER
 
 # Create one user
-if [ $(getent passwd $PUID) ] ; then
+if getent passwd "$PUID" >/dev/null ; then
     echo "UID $PUID already exists, not creating $USER_NAME test user";
 else
     if [ -z "$USER_NAME" ]; then
         echo "Empty 'USER_NAME' variables, not creating test user";
     else
-        groupadd -g $PGID $USER_NAME
-        useradd -m -s /bin/bash -N -u $PUID -g $PGID $USER_NAME
+        groupadd -g "$PGID" "$USER_NAME"
+        useradd -m -s /bin/bash -N -u "$PUID" -g "$PGID" "$USER_NAME"
 
         # TODO: make sure that $USER_NAME replaces azureuser in /etc/rstudio/nginx.site.conf
 
@@ -72,14 +72,14 @@ PWB_LAUNCHER_TIMEOUT=${PWB_LAUNCHER_TIMEOUT:-${RSW_LAUNCHER_TIMEOUT:-60}}
 # Start Launcher
 if [ "$PWB_LAUNCHER" == "true" ]; then
   echo "Waiting for launcher to startup... to disable set PWB_LAUNCHER=false"
-  wait-for-it.sh localhost:5559 -t $PWB_LAUNCHER_TIMEOUT
+  wait-for-it.sh localhost:5559 -t "$PWB_LAUNCHER_TIMEOUT"
 fi
 
 # Check diagnostic configurations
 if [ "$DIAGNOSTIC_ENABLE" == "true" ]; then
   verify_installation
   if [ "$DIAGNOSTIC_ONLY" == "true" ]; then
-    echo $(<$DIAGNOSTIC_DIR/verify.log);
+    cat "$DIAGNOSTIC_DIR"/verify.log;
     echo "Exiting script because DIAGNOSTIC_ONLY=${DIAGNOSTIC_ONLY}";
     exit 0
   fi;
